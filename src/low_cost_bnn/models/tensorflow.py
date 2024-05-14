e2cd93deae8f5c93bbb546fc315c693c8d4779ae
Aaron Ho <aaronkho@mit.edu>
2024-05-14 17:12:58 -0400

    Reconfigure tensorflow model to allow saving in Keras format

--- src/low_cost_bnn/models/tensorflow.py
+++ src/low_cost_bnn/models/tensorflow.py
@@ -1,23 +1,36 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Concatenate, Dense, Lambda, LeakyReLU
+from tensorflow.keras.layers import Layer, Input, Concatenate, Dense, Lambda, LeakyReLU
 from tensorflow.keras.models import Model
 from tensorflow_probability import distributions as tfd
 from tensorflow_probability import layers as tfpl
 
-# Required conversion function
-def mean_dist_fn(variational_layer):
 
-    def mean_dist(inputs):
-        bias_mean = variational_layer.bias_posterior.mean()
-        kernel_mean = variational_layer.kernel_posterior.mean()
-        kernel_std = variational_layer.kernel_posterior.stddev()
-        mu_mean = tf.matmul(inputs, kernel_mean) + bias_mean
-        mu_var = tf.matmul(inputs ** 2, kernel_std ** 2)
-        mu_std = tf.sqrt(mu_var)
-        return tfd.Normal(mu_mean, mu_std)
+# Helper function to generate output distributions
+def create_normal_distributions(moments):
+    return tfd.Normal(loc=moments[..., :1], scale=moments[..., 1:])
 
-    return mean_dist
+
+# Required custom class for variational layer
+class DenseEpistemicLayer(tfpl.DenseReparameterization):
+
+    def __init__(self, *args, **kwargs):
+        super(DenseEpistemicLayer, self).__init__(*args, **kwargs)
+
+    def _compute_mean_distribution_moments(self, inputs):
+        kernel_mean = self.kernel_posterior.mean()
+        kernel_stddev = self.kernel_posterior.stddev()
+        bias_mean = self.bias_posterior.mean()
+        dist_mean = tf.matmul(inputs, kernel_mean) + bias_mean
+        dist_var = tf.matmul(inputs ** 2, kernel_stddev ** 2)
+        dist_stddev = tf.sqrt(dist_var)
+        return dist_mean, dist_stddev
+
+    def call(self, inputs):
+        samples = super(DenseEpistemicLayer, self).call(inputs)
+        means, stddevs = self._compute_mean_distribution_moments(inputs)
+        return means, stddevs, samples
+
 
 #Model architecture - should this be a class inheriting Model instead?
 def create_model(n_inputs, n_hidden, n_outputs, n_specialized=None):
@@ -29,28 +42,34 @@ def create_model(n_inputs, n_hidden, n_outputs, n_specialized=None):
         for ii in range(n_outputs):
             n_special[ii] = n_specialized[ii] if ii < len(n_specialized) else n_specialized[-1]
 
-    input_layer = Input(shape=(n_inputs, ))
-    hidden_layer = Dense(n_hidden, activation=leaky_relu)(input_layer)
+    inputs = Input(shape=(n_inputs,))
+    commons = Dense(n_hidden, activation=leaky_relu)(inputs)
 
-    specialized_layers = [None] * n_outputs
-    variational_objects = [None] * n_outputs
-    aleatoric_layers = [None] * n_outputs
-    epistemic_layers = [None] * n_outputs
-
-    output_layer = [None] * (2 * n_outputs)
+    outputs = [None] * (2 * n_outputs)
 
     for ii in range(n_outputs):
 
-        variational_objects[ii] = tfpl.DenseReparameterization(1, name=f'mu{ii}')
-        specialized_layers[ii] = Dense(n_special[ii], activation=leaky_relu)(hidden_layer)
-        aleatoric_layers[ii] = Dense(1, activation='softplus', name=f'sigma{ii}')(specialized_layers[ii])
-        epistemic_layers[ii] = variational_objects[ii](specialized_layers[ii])
+        specials = Dense(n_special[ii], activation=leaky_relu, name=f'specialized{ii}')(commons)
 
-        model_dist = tfpl.DistributionLambda(mean_dist_fn(variational_objects[ii]), name=f'output{ii}')(specialized_layers[ii])
-        noise_dist = tfpl.DistributionLambda(lambda p: tfd.Normal(p[0],p[1]))((epistemic_layers[ii], aleatoric_layers[ii]))
+        #variational_object = DenseEpistemicLayer(1, name=f'epistemic{ii}')
+        epistemic_means, epistemic_stddevs, sample_means = DenseEpistemicLayer(1, name=f'model{ii}')(specials)
+        aleatoric_stddevs = Dense(1, activation='softplus', name=f'noise{ii}')(specials)
 
-        output_layer[2*ii] = model_dist
-        output_layer[2*ii+1] = noise_dist
+        epistemics = Concatenate(name=f'epistemic{ii}')([epistemic_means, epistemic_stddevs])
+        aleatorics = Concatenate(name=f'aleatoric{ii}')([sample_means, aleatoric_stddevs])
 
-    return Model(input_layer, output_layer)
+        model_dist = tfpl.DistributionLambda(
+            make_distribution_fn=create_normal_distributions,
+            name=f'model_output{ii}'
+        )(epistemics)
+        noise_dist = tfpl.DistributionLambda(
+            make_distribution_fn=create_normal_distributions,
+            name=f'noise_output{ii}'
+        )(aleatorics)
+
+        # These are distributions, not tensors
+        outputs[2*ii] = model_dist
+        outputs[2*ii+1] = noise_dist
+
+    return Model(inputs, outputs)
 
