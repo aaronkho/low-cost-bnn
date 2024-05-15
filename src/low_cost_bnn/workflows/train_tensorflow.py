376ed6c840c80af83a7f40115d54aea3f6923e1e
Aaron Ho <aaronkho@mit.edu>
2024-05-14 21:19:16 -0400

    Bugfixes for prior construction, added logging capabilities

--- src/low_cost_bnn/workflows/train_tensorflow.py
+++ src/low_cost_bnn/workflows/train_tensorflow.py
@@ -1,5 +1,7 @@
+import sys
 import argparse
 import time
+import logging
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -8,6 +10,8 @@ from tensorflow_probability import distributions as tfd
 from ..models.tensorflow import create_model
 from ..utils.helpers import create_scaler, split
 from ..utils.helpers_tensorflow import create_data_loader, create_learning_rate_scheduler, create_adam_optimizer
+
+logger = logging.getLogger("train_tensorflow")
 
 def parse_inputs():
     parser = argparse.ArgumentParser()
@@ -34,7 +38,35 @@ def parse_inputs():
     parser.add_argument('--learning_rate', metavar='rate', type=float, default=0.001, help='Initial learning rate for Adam optimizer')
     parser.add_argument('--decay_rate', metavar='rate', type=float, default=0.98, help='Scheduled learning rate decay for Adam optimizer')
     parser.add_argument('--decay_epochs', metavar='n', type=float, default=20, help='Epochs between applying learning rate decay for Adam optimizer')
+    parser.add_argument('--log_file', metavar='path', type=str, default=None, help='Optional path to log file where script related print outs will be stored')
+    parser.add_argument('-v', dest='verbosity', action='count', default=0, help='Set level of verbosity for the training script')
     return parser.parse_args()
+
+
+def print_settings(args):
+    logger.debug(f'Input settings print-out requested...')
+    for key, val in vars(args).items():
+       logger.debug(f'  {key}: {val}')
+
+
+def setup_logging(log_file=None, verbosity=0):
+
+    formatter = logging.Formatter('%(name)s - %(levelname)s: %(message)s')
+    logger.setLevel(logging.INFO)
+    if verbosity >= 1:
+        logger.setLevel(logging.DEBUG)
+
+    if isinstance(log_file, str):
+        log = logging.FileHandler(log_file, mode='w')
+        log.setLevel(logging.DEBUG)
+        log.setFormatter(formatter)
+        logger.addHandler(log)
+
+    else:
+        stream = logging.StreamHandler(sys.stdout)
+        stream.setLevel(logging.DEBUG)
+        stream.setFormatter(formatter)
+        logger.addHandler(stream)
 
 
 def preprocess_data(
@@ -44,7 +76,8 @@ def preprocess_data(
     validation_fraction,
     test_fraction,
     shuffle=True,
-    seed=None
+    seed=None,
+    verbosity=0
 ):
 
     ml_vars = []
@@ -54,6 +87,11 @@ def preprocess_data(
 
     feature_scaler = create_scaler(ml_data.loc[:, feature_vars])
     target_scaler = create_scaler(ml_data.loc[:, target_vars])
+    if verbosity >= 2:
+        logger.debug(f'  Input scaling mean: {feature_scaler.mean_}')
+        logger.debug(f'  Input scaling std: {feature_scaler.scale_}')
+        logger.debug(f'  Output scaling mean: {target_scaler.mean_}')
+        logger.debug(f'  Output scaling std: {target_scaler.scale_}')
 
     first_split = validation_fraction + test_fraction
     second_split = test_fraction / first_split
@@ -97,7 +135,8 @@ def ncp_train_epoch(
     epistemic_weights,
     aleatoric_weights,
     ood_sigmas,
-    ood_seed=None
+    ood_seed=None,
+    verbosity=0
 ):
 
     step_total_losses = tf.TensorArray(dtype=tf.dtypes.float32, size=0, dynamic_size=True, clear_after_read=True, name=f'total_loss_array')
@@ -106,14 +145,14 @@ def ncp_train_epoch(
     step_aleatoric_losses = tf.TensorArray(dtype=tf.dtypes.float32, size=0, dynamic_size=True, clear_after_read=True, name=f'alea_loss_array')
 
     # Training loop through minibatches - each loop pass is one step
-    for feature_batch, target_batch, epistemic_sigma_batch, aleatoric_sigma_batch in dataloader:
+    for nn, (feature_batch, target_batch, epistemic_sigma_batch, aleatoric_sigma_batch) in enumerate(dataloader):
 
         # Define epistemic prior for NCP methodology
         epistemic_priors = []
         for ii in range(target_batch.shape[1]):
             target_reshape = tf.expand_dims(target_batch[:, ii], axis=-1)
             sigma_reshape = tf.expand_dims(epistemic_sigma_batch[:, ii], axis=-1)
-            prior = tfd.Normal(target_reshape, sigma_reshape, name=f'EpiNormal{ii}')
+            prior = tfd.Normal(target_reshape, sigma_reshape, name=f'EpiPrior{ii}')
             epistemic_priors.append(prior)
 
         # Define aleatoric prior for NCP methodology
@@ -121,7 +160,7 @@ def ncp_train_epoch(
         for ii in range(target_batch.shape[1]):
             target_reshape = tf.expand_dims(target_batch[:, ii], axis=-1)
             sigma_reshape = tf.expand_dims(aleatoric_sigma_batch[:, ii], axis=-1)
-            prior = tfd.Normal(target_reshape, sigma_reshape, name=f'AleaNormal{ii}')
+            prior = tfd.Normal(target_reshape, sigma_reshape, name=f'AleaPrior{ii}')
             aleatoric_priors.append(prior)
         
         # Generate random OOD data from training data
@@ -144,39 +183,40 @@ def ncp_train_epoch(
             ood_epistemic_dists = ood_outputs[::2]
             ood_aleatoric_dists = ood_outputs[1::2]
 
+            if tf.executing_eagerly() and verbosity >= 4:
+                for ii in range(len(mean_epistemic_dists)):
+                    logger.debug(f'     In-dist model: {mean_epistemic_dists[ii].mean()[0]}, {mean_epistemic_dists[ii].stddev()[0]}')
+                    logger.debug(f'     In-dist noise: {mean_aleatoric_dists[ii].mean()[0]}, {mean_aleatoric_dists[ii].stddev()[0]}')
+                    logger.debug(f'     Out-of-dist model: {ood_epistemic_dists[ii].mean()[0]}, {ood_epistemic_dists[ii].stddev()[0]}')
+                    logger.debug(f'     Out-of-dist noise: {ood_aleatoric_dists[ii].mean()[0]}, {ood_aleatoric_dists[ii].stddev()[0]}')
+
             # Container for total loss
             total_loss = tf.constant([0.0], dtype=tf.dtypes.float32, name='total_loss_stack') * target_batch[:, 0]
 
             # Negative log-likelihood loss term: compare probability of target against mean noise distribution
-            #likelihood_bases = [np.nan] * len(mean_aleatoric_dists)
             likelihood_terms = [np.nan] * len(mean_aleatoric_dists)
             for ii in range(len(mean_aleatoric_dists)):
                 nll_targets = tf.expand_dims(target_batch[:, ii], axis=-1)
                 negative_log_likelihood = -mean_aleatoric_dists[ii].log_prob(nll_targets, name=f'NLL{ii}')
                 likelihood_base = tf.reshape(negative_log_likelihood, tf.shape(total_loss))
-                #likelihood_bases[ii] = tf.reduce_sum(likelihood_base)
                 likelihood_terms[ii] = tf.reduce_sum(tf.math.multiply(likelihood_weights[ii], likelihood_base))
                 total_loss = total_loss + tf.math.multiply(likelihood_weights[ii], likelihood_base, name=f'nll_weight')
             step_likelihood_loss = tf.stack(likelihood_terms, name=f'nll_loss_stack')
 
             # Epistemic loss term: KL-divergence between model prior and OOD model distribution
-            #epistemic_bases = [np.nan] * len(ood_epistemic_dists)
             epistemic_terms = [np.nan] * len(ood_epistemic_dists)
             for ii in range(len(ood_epistemic_dists)):
                 ood_epistemic_kldiv = tfd.kl_divergence(epistemic_priors[ii], ood_epistemic_dists[ii], name=f'EpiKLD{ii}')
                 epistemic_base = tf.reshape(ood_epistemic_kldiv, tf.shape(total_loss))
-                #epistemic_bases[ii] = tf.reduce_sum(epistemic_base)
                 epistemic_terms[ii] = tf.reduce_sum(tf.math.multiply(epistemic_weights[ii], epistemic_base))
                 total_loss = total_loss + tf.math.multiply(epistemic_weights[ii], epistemic_base, name=f'epi_weight')
             step_epistemic_loss = tf.stack(epistemic_terms, name=f'epi_loss_stack')
 
             # Aleatoric loss term: KL-divergence between noise prior and OOD noise distribution
-            #aleatoric_bases = [np.nan] * len(ood_aleatoric_dists)
             aleatoric_terms = [np.nan] * len(ood_aleatoric_dists)
             for ii in range(len(ood_aleatoric_dists)):
                 ood_aleatoric_kldiv = tfd.kl_divergence(aleatoric_priors[ii], ood_aleatoric_dists[ii], name=f'AleaKLD{ii}')
                 aleatoric_base = tf.reshape(ood_aleatoric_kldiv, tf.shape(total_loss))
-                #aleatoric_bases[ii] = tf.reduce_sum(aleatoric_base)
                 aleatoric_terms[ii] = tf.reduce_sum(tf.math.multiply(aleatoric_weights[ii], aleatoric_base))
                 total_loss = total_loss + tf.math.multiply(aleatoric_weights[ii], aleatoric_base, name=f'alea_weight')
             step_aleatoric_loss = tf.stack(aleatoric_terms, name=f'alea_loss_stack')
@@ -190,10 +230,16 @@ def ncp_train_epoch(
         optimizer.apply_gradients(zip(gradients, trainable_vars))
 
         # Accumulate batch losses to determine epoch loss
-        step_total_losses.write(step_total_losses.size(), step_total_loss)
-        step_likelihood_losses.write(step_likelihood_losses.size(), step_likelihood_loss)
-        step_epistemic_losses.write(step_likelihood_losses.size(), step_epistemic_loss)
-        step_aleatoric_losses.write(step_aleatoric_losses.size(), step_aleatoric_loss)
+        fill_index = tf.cast(nn + 1, tf.int32)
+        step_total_losses = step_total_losses.write(fill_index, step_total_loss)
+        step_likelihood_losses = step_likelihood_losses.write(fill_index, step_likelihood_loss)
+        step_epistemic_losses = step_epistemic_losses.write(fill_index, step_epistemic_loss)
+        step_aleatoric_losses = step_aleatoric_losses.write(fill_index, step_aleatoric_loss)
+
+        if tf.executing_eagerly() and verbosity >= 3:
+            logger.debug(f'  - Batch {nn + 1}: total = {step_total_loss:.3f}')
+            for ii in range(len(mean_epistemic_dists)):
+                logger.debug(f'     Output {ii}: nll = {step_likelihood_loss[ii]:.3f}, epi = {step_epistemic_loss[ii]:.3f}, alea = {step_aleatoric_loss[ii]:.3f}')
 
     epoch_total_loss = tf.reduce_sum(step_total_losses.stack(), axis=0)
     epoch_likelihood_loss = tf.reduce_sum(step_likelihood_losses.stack(), axis=0)
@@ -219,13 +265,20 @@ def train(
     alea_weights,
     batch_size=None,
     patience=None,
-    seed=None
+    seed=None,
+    verbosity=0
 ):
 
     n_inputs = features_train.shape[1]
     n_outputs = targets_train.shape[1]
     train_length = features_train.shape[0]
-    #valid_length = features_valid.shape[0]
+    valid_length = features_valid.shape[0]
+
+    if verbosity >= 1:
+        logger.info(f' Number of inputs: {n_inputs}')
+        logger.info(f' Number of outputs: {n_outputs}')
+        logger.info(f' Training set size: {train_length}')
+        logger.info(f' Validation set size: {valid_length}')
 
     # Assume standardized OOD distribution width based on entire feature value range - better to use quantiles?
     ood_sigma = [ood_width] * n_inputs
@@ -233,8 +286,8 @@ def train(
         ood_sigma[jj] = ood_sigma[jj] * float(np.nanmax(features_train[:, jj]) - np.nanmin(features_train[:, jj]))
 
     # Create data loaders, including minibatching for training set
-    train_data = (features_train, targets_train, epi_priors, alea_priors)
-    valid_data = (features_valid, targets_valid)
+    train_data = (features_train.astype(np.float32), targets_train.astype(np.float32), epi_priors.astype(np.float32), alea_priors.astype(np.float32))
+    valid_data = (features_valid.astype(np.float32), targets_valid.astype(np.float32))
     train_loader = create_data_loader(train_data, buffer_size=train_length, seed=seed, batch_size=batch_size)
     valid_loader = create_data_loader(valid_data)
 
@@ -285,7 +338,8 @@ def train(
             epi_weight_tensors,
             alea_weight_tensors,
             ood_sigma,
-            ood_seed=None
+            ood_seed=None,
+            verbosity=verbosity
         )
 
         train_outputs = model(features_train, training=False)
@@ -321,10 +375,15 @@ def train(
         mae_list.append(mae)
         mse_list.append(mse)
 
-        if (epoch + 1) % 100 == 0:
-            print(f'Epoch {epoch + 1}: total = {total_list[-1]:.3f}')
+        print_per_epochs = 100
+        if verbosity >= 2:
+            print_per_epochs = 10
+        if verbosity >= 3:
+            print_per_epochs = 1
+        if (epoch + 1) % print_per_epochs == 0:
+            logger.info(f' Epoch {epoch + 1}: total = {total_list[-1]:.3f}')
             for ii in range(n_outputs):
-                print(f'  Output {ii}: mse = {mse_list[-1][ii]:.3f}, mae = {mae_list[-1][ii]:.3f}, nll = {nll_list[-1][ii]:.3f}, epi = {epi_list[-1][ii]:.3f}, alea = {alea_list[-1][ii]:.3f}')
+                logger.debug(f'  Output {ii}: mse = {mse_list[-1][ii]:.3f}, mae = {mae_list[-1][ii]:.3f}, nll = {nll_list[-1][ii]:.3f}, epi = {epi_list[-1][ii]:.3f}, alea = {alea_list[-1][ii]:.3f}')
 
         total_tracker.reset_states()
         for ii in range(n_outputs):
@@ -346,11 +405,17 @@ def train(
 def main():
 
     args = parse_inputs()
+    setup_logging(args.log_file, args.verbosity)
+    if args.verbosity >= 2:
+        print_settings(args)
 
     ipath = Path(args.data_file)
     mpath = Path(args.metrics_file)
     npath = Path(args.network_file)
     if ipath.is_file():
+
+        if args.verbosity >= 2:
+            tf.config.run_functions_eagerly(True)
 
         # Set up the required data sets
         start_preprocess = time.perf_counter()
@@ -361,11 +426,12 @@ def main():
             args.output_vars,
             args.validation_fraction,
             args.test_fraction,
-            seed=args.shuffle_seed
+            seed=args.shuffle_seed,
+            verbosity=args.verbosity
         )
         end_preprocess = time.perf_counter()
 
-        print(f'Pre-processing completed! Elpased time: {(end_preprocess - start_preprocess):.4f} s')
+        logger.info(f'Pre-processing completed! Elpased time: {(end_preprocess - start_preprocess):.4f} s')
 
         # Set up the BNN-NCP model
         start_setup = time.perf_counter()
@@ -375,22 +441,27 @@ def main():
             n_inputs=n_inputs,
             n_hidden=args.hidden_nodes,
             n_outputs=n_outputs,
-            n_specialized=args.specialized_nodes
+            n_specialized=args.specialized_nodes,
+            verbosity=args.verbosity
         )
 
         # Set up the user-defined prior factors
-        epi_priors = 0.001 * features['original']
+        epi_priors = 0.001 * features['original'] / features['scaler'].scale_
         for ii in range(n_outputs):
             epi_factor = 0.001
             if isinstance(args.epi_prior, list):
                 epi_factor = args.epi_prior[ii] if ii < len(args.epi_prior) else args.epi_prior[-1]
-            epi_priors[:, ii] = epi_factor * features['original'][:, ii]
-        alea_priors = 0.001 * features['original']
+            epi_priors[:, ii] = np.abs(epi_factor * features['original'][:, ii] / features['scaler'].scale_[ii])
+        alea_priors = 0.001 * features['original'] / features['scaler'].scale_
         for ii in range(n_outputs):
             alea_factor = 0.001
             if isinstance(args.alea_prior, list):
                 alea_factor = args.alea_prior[ii] if ii < len(args.alea_prior) else args.alea_prior[-1]
-            alea_priors[:, ii] = alea_factor * features['original'][:, ii]
+            alea_priors[:, ii] = np.abs(alea_factor * features['original'][:, ii] / features['scaler'].scale_[ii])
+
+        # Required minimum priors to avoid infs and nans in KL-divergence
+        epi_priors[epi_priors < 1.0e-6] = 1.0e-6
+        alea_priors[alea_priors < 1.0e-6] = 1.0e-6
 
         # Set up the user-defined loss term weights
         nll_weights = [1.0] * n_outputs
@@ -417,7 +488,7 @@ def main():
         optimizer = create_adam_optimizer(scheduler)
         end_setup = time.perf_counter()
 
-        print(f'Setup completed! Elapsed time: {(end_setup - start_setup):.4f} s')
+        logger.info(f'Setup completed! Elapsed time: {(end_setup - start_setup):.4f} s')
 
         # Perform the training loop
         start_train = time.perf_counter()
@@ -437,11 +508,12 @@ def main():
             alea_weights,
             batch_size=args.batch_size,
             patience=args.early_stopping,
-            seed=args.sample_seed
+            seed=args.sample_seed,
+            verbosity=args.verbosity
         )
         end_train = time.perf_counter()
 
-        print(f'Training loop completed! Elapsed time: {(end_train - start_train):.4f} s')
+        logger.info(f'Training loop completed! Elapsed time: {(end_train - start_train):.4f} s')
 
         # Save the trained model and training metrics
         start_save = time.perf_counter()
@@ -459,13 +531,16 @@ def main():
             metric_dict[f'epi{ii}'] = epistemic[:, ii].flatten()
             metric_dict[f'alea{ii}'] = aleatoric[:, ii].flatten()
         metrics = pd.DataFrame(data=metric_dict)
-        metrics.to_hdf(mpath, '/data')
+        metrics.to_hdf(mpath, key='/data')
         model.save(npath)
         end_save = time.perf_counter()
 
-        print(f'Saving completed! Elapsed time: {(end_save - start_save):.4f} s')
+        logger.info(f'Saving completed! Elapsed time: {(end_save - start_save):.4f} s')
 
-        print(f'Script completed: Network saved in {npath}, metrics saved in {mpath}')
+        logger.info(f' Metrics saved in {mpath}')
+        logger.info(f' Network saved in {npath}')
+
+        logger.info(f'Script completed!')
 
     else:
         raise IOError(f'Could not find input file: {ipath}')
