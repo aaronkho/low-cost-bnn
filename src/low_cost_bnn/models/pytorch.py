2f70dd91564e410b0bb00e333c0922b316a57fd6
Aaron Ho <aaronkho@mit.edu>
2024-05-16 18:43:13 -0400

    PyTorch version of BNN-NCP model constructued, can be loaded but requires testing

--- src/low_cost_bnn/models/pytorch.py
+++ src/low_cost_bnn/models/pytorch.py
@@ -1,6 +1,22 @@
+import math
 import torch
-from torch.nn import Parameter, Module, ModuleDict, Sequential, Linear, LeakyReLU, SoftPlus
+from torch.nn import Parameter, Module, ModuleDict, Sequential, Linear, LeakyReLU, Softplus, init
 import torch.distributions as tnd
+
+
+class NullDistribution():
+
+    def __init__(self, null_value=None):
+        self.null_value = null_value
+
+    def sample(self):
+        return self.null_value
+
+    def mean(self):
+        return self.null_value
+
+    def stddev(self):
+        return self.null_value
 
 
 class EpistemicLayer(Module):
@@ -9,12 +25,11 @@ class EpistemicLayer(Module):
         self,
         in_features,
         out_features,
-        kernel_prior_fn,
-        kernel_posterior_fn=tnd.multivariate_normal.MultivariateNormal,
-        kernel_divergence_fn=torch.nn.functional.kl_div,
-        bias_prior_fn,
-        bias_posterior_fn,
-        bias_divergence_fn=torch.nn.functional.kl_div,
+        bias=True,
+        kernel_prior=True,
+        bias_prior=False,
+        kernel_divergence_fn=tnd.kl.kl_divergence,
+        bias_divergence_fn=tnd.kl.kl_divergence,
         device=None,
         dtype=None
     ):
@@ -23,37 +38,93 @@ class EpistemicLayer(Module):
         super(EpistemicLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.kernel_prior_fn = kernel_prior_fn
-        self.kernel_posterior_fn = kernel_posterior_fn
+
+        self.kernel_loc = Parameter(torch.empty((self.in_features, self.out_features), **self.factory_kwargs))
+        self.kernel_scale = Parameter(torch.empty((self.in_features, self.out_features), **self.factory_kwargs))
+        self.use_kernel_prior = kernel_prior
+        self.use_bias_prior = bias_prior
+
+        if bias:
+            self.bias_loc = Parameter(torch.empty(self.out_features, **self.factory_kwargs))
+            self.bias_scale = Parameter(torch.empty(self.out_features, **self.factory_kwargs))
+        else:
+            self.register_parameter('bias_loc', None)
+            self.register_parameter('bias_scale', None)
+
         self.kernel_divergence_fn = kernel_divergence_fn
-        self.bias_prior_fn = bias_prior_fn
-        self.bias_posterior_fn = bias_posterior_fn
         self.bias_divergence_fn = bias_divergence_fn
+
+        self.reset_parameters()
+        self.build()
+
+
+    def reset_parameters(self):
+        init.kaiming_normal_(self.kernel_loc, a=math.sqrt(5))
+        fan_in, fan_out = init._calculate_fan_in_and_fan_out(self.kernel_scale)
+        bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+        init.uniform_(self.kernel_scale, 0.01 * bound, bound)
+        if self.bias_loc is not None:
+            fan_in, fan_out = init._calculate_fan_in_and_fan_out(self.kernel_loc)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            init.uniform_(self.bias_loc, -1.0 * bound, bound)
+        if self.bias_scale is not None:
+            fan_in, fan_out = init._calculate_fan_in_and_fan_out(self.kernel_scale)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            init.uniform_(self.bias_scale, 0.01 * bound, bound)
 
 
     def build(self):
 
-        self.kernel_posterior = Parameter(self.kernel_posterior_fn(
-            self.inputs,
-            self.outputs,
-            **self.factory_kwargs
-        )
+        layer_shape = (self.in_features, self.out_features)
 
-        if self.kernel_prior_fn is None:
-            self.kernel_prior = None
+        self.kernel_posterior = tnd.independent.Independent(tnd.normal.Normal(self.kernel_loc, self.kernel_scale), 2)
+
+        if self.use_kernel_prior:
+            self.kernel_prior = tnd.independent.Independent(tnd.normal.Normal(torch.zeros(layer_shape), torch.ones(layer_shape)), 2)
         else:
-            self.kernel_prior = self.kernel_prior_fn(
-                self.inputs,
-                self.outputs,
-                **self.factory_kwargs
-            )
+            self.kernel_prior = NullDistribution(None)
 
-        self.bias_prior = self.bias_prior()
-        self.bias_posterior = self.bias_posterior()
+        if self.bias_loc is not None and self.bias_scale is not None:
+            self.bias_posterior = tnd.independent.Independent(tnd.normal.Normal(self.bias_loc, self.bias_scale), 1)
+        else:
+            self.bias_posterior = NullDistribution(None)
+
+        if self.use_bias_prior:
+            self.bias_prior = tnd.independent.Independent(tnd.normal.Normal(torch.zeros(self.out_features), torch.ones(self.out_features)), 1)
+        else:
+            self.bias_prior = NullDistribution(None)
+
+
+    def _apply_divergence(self, divergence_fn, posterior, prior):
+        loss = None
+        if divergence_fn is not None and not isinstance(posterior, NullDistribution) and not isinstance(prior, NullDistribution):
+            loss = divergence_fn(prior, posterior)
+        return loss
+
+
+    def _compute_mean_distribution_moments(self, inputs):
+        kernel_mean = self.kernel_posterior.mean()
+        kernel_stddev = self.kernel_posterior.stddev()
+        bias_mean = self.bias_posterior.mean()
+        dist_mean = torch.nn.functional.linear(inputs, kernel_mean, bias=bias_mean)
+        dist_var = torch.nn.functional.linear(inputs ** 2, kernel_stddev ** 2)
+        dist_stddev = torch.sqrt(dist_var)
+        return dist_mean, dist_stddev
 
 
     def forward(self, inputs):
+        kernel_posterior_tensor = self.kernel_posterior.sample()
+        bias_posterior_tensor = self.bias_posterior.sample()
+        samples = torch.nn.functional.linear(inputs, kernel_posterior_tensor, bias=bias_posterior_tensor)
+        means, stddevs = self._compute_mean_distribution_moments(inputs)
         return means, stddevs, samples
+
+
+    # Not sure if these are actually used in TensorFlow-equivalent model
+    def get_divergence_losses(self):
+        kernel_divergence_loss = self._apply_divergence(self.kernel_divergence_fn, self.kernel_posterior, self.kernel_prior)
+        bias_divergence_loss = self._apply_divergence(self.bias_divergence_fn, self.bias_posterior, self.bias_prior)
+        return kernel_divergence_loss, bias_divergence_loss
 
 
 class LowCostBNN(Module):
@@ -68,19 +139,19 @@ class LowCostBNN(Module):
         if isinstance(n_specialized, (list, tuple)):
             for ii in range(n_outputs):
                 self.n_special[ii] = n_specialized[ii] if ii < len(n_specialized) else n_specialized[-1]
-        build()
+        self.build()
 
 
-    def build():
+    def build(self):
 
-        self.linear_activation = LeakyReLU(negative_slope=0.2)
-        self.aleatoric_activation = SoftPlus()
+        self.leaky_relu = LeakyReLU(negative_slope=0.2)
+        self.softplus = Softplus()
 
         self.common_layers = Linear(self.n_inputs, self.n_hidden)
         self.special_layers = ModuleDict()
         self.epistemic_layers = ModuleDict()
         self.aleatoric_layers = ModuleDict()
-        for ii in range(n_outputs):
+        for ii in range(self.n_outputs):
             self.special_layers.update({f'specialized{ii}': Linear(self.n_hidden, self.n_special[ii])})
             self.epistemic_layers.update({f'model{ii}': EpistemicLayer(self.n_special[ii], 1)})
             self.aleatoric_layers.update({f'noise{ii}': Linear(self.n_special[ii], 1)})
@@ -90,14 +161,13 @@ class LowCostBNN(Module):
 
         outputs = [None] * (2 * self.n_outputs)
 
-        commons = self.common_layers(inputs)
-        commons = self.linear_activation(commons)
+        commons = self.leaky_relu(self.common_layers(inputs))
 
         for ii in range(self.n_outputs):
 
-            specials = self.special_layers[f'specialized{ii}'](commons)
+            specials = self.leaky_relu(self.special_layers[f'specialized{ii}'](commons))
             epistemic_means, epistemic_stddevs, sample_means = self.epistemic_layers[f'model{ii}'](specials)
-            aleatoric_stddevs = self.aleatoric_layers[f'noise{ii}'](specials)
+            aleatorics_stddevs = self.softplus(self.aleatoric_layers[f'noise{ii}'](specials))
 
             model_dist = tnd.normal.Normal(epistemic_means, epistemic_stddevs)
             noise_dist = tnd.normal.Normal(sample_means, aleatoric_stddevs)
@@ -106,3 +176,20 @@ class LowCostBNN(Module):
             outputs[2*ii+1] = tnd.independent.Independent(noise_dist, 1)
 
         return outputs
+
+
+    def get_divergence_losses(self):
+        losses = torch.zeros(torch.Size([self.n_outputs, 2]))
+        for ii in range(self.n_outputs):
+            if isinstance(self.epistemic_layers[f'model{ii}'], EpistemicLayer):
+                kernel_loss, bias_loss = self.epistemic_layers[f'model{ii}'].get_divergence_losses()
+                if kernel_loss is not None:
+                    losses[ii, 0] = kernel_loss
+                if bias_loss is not None and bias_loss is not None:
+                    losses[ii, 1] = bias_loss
+        return losses
+
+
+# Should re-order arguments to allow built-in hidden layer flexibility
+def create_model(n_inputs, n_hidden, n_outputs, n_specialized=None, verbosity=0):
+    return LowCostBNN(n_inputs, n_outputs, n_hidden, n_specialized)
