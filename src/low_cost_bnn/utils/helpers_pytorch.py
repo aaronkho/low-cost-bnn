013b5e2a19f9ec1a578405555c4d001919919dc1
Aaron Ho <aaronkho@mit.edu>
2024-05-20 17:20:51 -0400

    Bugfixes on Pytorch-specific helper functions

--- src/low_cost_bnn/utils/helpers_pytorch.py
+++ src/low_cost_bnn/utils/helpers_pytorch.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 def create_data_loader(data_tuple, batch_size=None, buffer_size=None, seed=None):
-    dataset = torch.utils.data.TensorDataset(data_tuple)
+    dataset = torch.utils.data.TensorDataset(*data_tuple)
     shuffle = True if isinstance(buffer_size, int) else None
     generator = None
     if isinstance(seed, int):
@@ -12,7 +12,7 @@ def create_data_loader(data_tuple, batch_size=None, buffer_size=None, seed=None)
     return loader
 
 
-def generate_scheduled_adam_optimizer(model, learning_rate, decay_steps, decary_rate):
+def create_scheduled_adam_optimizer(model, learning_rate, decay_steps, decay_rate):
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, decay_steps, gamma=decay_rate)
     return optimizer, scheduler
