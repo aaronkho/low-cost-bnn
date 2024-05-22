9b7bee97e220c110585cce10d1622811dc23c94f
Aaron Ho <aaron.k.hohoho@gmail.com>
2024-05-22 09:14:02 -0400

    Added custom loss function classes to TensorFlow implementation, needs testing

--- src/low_cost_bnn/models/tensorflow.py
+++ src/low_cost_bnn/models/tensorflow.py
@@ -50,7 +50,6 @@ def create_model(n_inputs, n_hidden, n_outputs, n_specialized=None, verbosity=0)
 
         specials = Dense(n_special[ii], activation=leaky_relu, name=f'specialized{ii}')(commons)
 
-        #variational_object = DenseEpistemicLayer(1, name=f'epistemic{ii}')
         epistemic_means, epistemic_stddevs, sample_means = DenseEpistemicLayer(1, name=f'model{ii}')(specials)
         aleatoric_stddevs = Dense(1, activation='softplus', name=f'noise{ii}')(specials)
 
@@ -75,13 +74,18 @@ def create_model(n_inputs, n_hidden, n_outputs, n_specialized=None, verbosity=0)
 
 class DistributionNLLLoss(tf.keras.losses.Loss):
 
-class EpistemicLoss(tf.keras.losses.Loss):
+    def __init__(self, name='nll', **kwargs):
+        super(DistributionNLLLoss, self).__init__(name=name, **kwargs)
 
-    def __init__(self, name='epistemic', **kwargs):
-        super(EpistemicLoss, self).__init__(name=name, **kwargs)
 
-    def call(self):
+    def call(self, targets, distributions):
+        loss = -distributions.log_prob(targets)
+        if self.reduction == 'mean':
+            loss = tf.reduce_mean(loss)
+        elif self.reduction == 'sum':
+            loss = tf.reduce_sum(loss)
         return loss
+
 
     def get_config(self):
         base_config = super(EpistemicLoss, self).get_config()
@@ -89,3 +93,134 @@ class EpistemicLoss(tf.keras.losses.Loss):
         }
         return {**base_config, **config}
 
+
+class DistributionKLDivLoss(tf.keras.losses.Loss):
+
+    def __init__(self, name='kld', **kwargs):
+        super(DistributionKLDivLoss, self).__init__(name=name, **kwargs)
+
+
+    def call(self, priors, posteriors):
+        loss = tfd.kl_divergence(priors, posteriors)
+        if self.reduction == 'mean':
+            loss = tf.reduce_mean(loss)
+        elif self.reduction == 'sum':
+            loss = tf.reduce_sum(loss)
+        return loss
+
+
+    def get_config(self):
+        base_config = super(EpistemicLoss, self).get_config()
+        config = {
+        }
+        return {**base_config, **config}
+
+
+class NoiseContrastivePriorLoss(tf.keras.losses.Loss):
+
+    def __init__(self, likelihood_weight=1.0, epistemic_weight=1.0, aleatoric_weight=1.0, name='ncp', reduction='sum', **kwargs):
+        super(NoiseContrastivePriorLoss, self).__init__(name=name, reduction=reduction, **kwargs)
+        self._likelihood_weight = likelihood_weight
+        self._epistemic_weight = epistemic_weight
+        self._aleatoric_weight = aleatoric_weight
+        self._likelihood_loss_fn = DistributionNLLLoss(name=self.name+'_nll', reduction=reduction)
+        self._epistemic_loss_fn = DistributionKLDivLoss(name=self.name+'_epi_kld', reduction=reduction)
+        self._aleatoric_loss_fn = DistributionKLDivLoss(name=self.name+'_alea_kld', reduction=reduction)
+
+
+    def _calculate_likelihood_loss(self, targets, predictions):
+        weight = tf.constant(self._likelihood_weight, dtype=self.dtype)
+        base = self._likelihood_loss_fn(targets, predictions)
+        loss = weight * base
+        return loss
+
+
+    def _calculate_likelihood_loss(self, targets, predictions):
+        weight = tf.constant(self._epistemic_weight, dtype=self.dtype)
+        base = self._epistemic_loss_fn(targets, predictions)
+        loss = weight * base
+        return loss
+
+
+    def _calculate_likelihood_loss(self, targets, predictions):
+        weight = tf.constant(self._aleatoric_weight, dtype=self.dtype)
+        base = self._aleatoric_loss_fn(targets, predictions)
+        loss = weight * base
+        return loss
+
+
+    def call(self, targets, prediction_dists, model_priors, model_posteriors, noise_priors, noise_posteriors):
+        likelihood_loss = self._calculate_likelihood_loss(targets, prediction_dists)
+        epistemic_loss = self._calculate_model_divergence_loss(model_priors, model_posteriors)
+        aleatoric_loss = self._calculate_noise_divergence_loss(noise_priors, noise_posteriors)
+        total_loss = likelihood_loss + epistemic_loss + aleatoric_loss
+        return total_loss
+
+
+    def get_config(self):
+        base_config = super(EpistemicLoss, self).get_config()
+        config = {
+            'likelihood_weight': self._likelihood_weight,
+            'epistemic_weight': self._epistemic_weight,
+            'aleatoric_weight': self._aleatoric_weight
+        }
+        return {**base_config, **config}
+
+class MultiOutputNoiseContrastivePriorLoss(tf.keras.losses.Loss):
+
+    def __init__(self, n_outputs, likelihood_weights, epistemic_weights, aleatoric_weights, name='ncp', reduction='sum', **kwargs):
+        super(MultiOutputNoiseContrastivePriorLoss, self).__init__(name=name, reduction=reduction, **kwargs)
+        self.n_outputs = n_outputs
+        self._loss_fns = [None] * self.n_outputs
+        for ii in range(self.n_outputs):
+            nll_w = 1.0
+            epi_w = 1.0
+            alea_w = 1.0
+            if isinstance(likelihood_weights, (list, tuple)):
+                nll_w = likelihood_weights[ii] if ii < len(likelihood_weights) else likelihood_weights[-1]
+            if isinstance(epistemic_weights, (list, tuple)):
+                epi_w = epistemic_weights[ii] if ii < len(epistemic_weights) else epistemic_weights[-1]
+            if isinstance(aleatoric_weights, (list, tuple)):
+                alea_w = aleatoric_weights[ii] if ii < len(aleatoric_weights) else aleatoric_weights[-1]
+            self._loss_fns[ii] = NoiseContrastivePriorLoss(nll_w, epi_w, alea_w, name=f'nll{ii}', reduction=None)
+
+
+    def _calculate_likelihood_loss(self, targets, predictions):
+        losses = []
+        for ii in range(self.n_outputs):
+            losses[ii] = self._loss_fns[ii]._calculate_likelihood_loss(targets[ii], predictions[ii])
+        return losses
+
+
+    def _calculate_model_divergence_loss(self, targets, predictions):
+        losses = torch.zeros(torch.Size([self.n_outputs]))
+        for ii in range(self.n_outputs):
+            losses[ii] = self._loss_fns[ii]._calculate_model_divergence_loss(targets[ii], predictions[ii])
+        return losses
+
+
+    def _calculate_noise_divergence_loss(self, targets, predictions):
+        losses = torch.zeros(torch.Size([self.n_outputs]))
+        for ii in range(self.n_outputs):
+            losses[ii] = self._loss_fns[ii]._calculate_noise_divergence_loss(inputs[ii], targets[ii])
+        return losses
+
+
+    def call(self, targets, prediction_dists, model_priors, model_posteriors, noise_priors, noise_posteriors):
+        likelihood_loss = self._calculate_likelihood_loss(targets, prediction_dists)
+        epistemic_loss = self._calculate_model_divergence_loss(model_priors, model_posteriors)
+        aleatoric_loss = self._calculate_noise_divergence_loss(noise_priors, noise_posteriors)
+        if self.reduction == 'mean':
+            likelihood_loss = torch.mean(likelihood_loss)
+            epistemic_loss = torch.mean(epistemic_loss)
+            aleatoric_loss = torch.mean(aleatoric_loss)
+        elif self.reduction == 'sum':
+            likelihood_loss = torch.sum(likelihood_loss)
+            epistemic_loss = torch.sum(epistemic_loss)
+            aleatoric_loss = torch.sum(aleatoric_loss)
+        total_loss = likelihood_loss + epistemic_loss + aleatoric_loss
+        return total_loss
+
+
+    def get_config(self):
+        base_config = super(
