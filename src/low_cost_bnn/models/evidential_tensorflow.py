0826c18b74c1558db12ae41a648c29f6aaf9df45
Aaron Ho <aaronkho@mit.edu>
2024-05-30 20:15:37 -0400

    Added preliminary evidential NN architecture, does not work yet

--- src/low_cost_bnn/models/evidential_tensorflow.py
+++ src/low_cost_bnn/models/evidential_tensorflow.py
@@ -1,35 +1,70 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Concatenate, Dense, LeakyReLU
+from tensorflow.keras.layers import Dense, LeakyReLU
 from tensorflow_probability import distributions as tfd
 
 
-def create_student_t_posterior(gamma, nu, alpha, beta, verbosity=0):
-    mean = gamma
-    sigma = tf.sqrt(beta * (1.0 + nu) / (nu * alpha))
-    dof = 2.0 * alpha
-    return tfd.StudentT(df=dof, loc=mean, scale=sigma)
+
+# ------ LAYERS ------
 
 
 class DenseReparameterizationNormalInverseGamma(tf.keras.layers.Layer):
 
+
+    _map = {
+        'gamma': 0,
+        'nu': 1,
+        'alpha': 2,
+        'beta': 3
+    }
+    _n_params = len(_map)
+    _n_recast_params = 3
+
+
     def __init__(self, units, **kwargs):
+
         super(DenseReparameterizationNormalInverseGamma, self).__init__(**kwargs)
+
         self.units = units
-        self.dense = Dense(4 * self.units, activation=None)
+        self.dense = Dense(self._n_params * self.units, activation=None)
+
+        self._n_outputs = self._n_params * self.units
+        self._n_recast_outputs = self._n_recast_params * self.units
 
 
+    # Output: Shape(batch_size, n_outputs)
+    @tf.function
     def call(self, inputs):
         outputs = self.dense(inputs)
-        gamma, lognu, logalpha, logbeta = tf.split(outputs, 4, axis=-1)
+        gamma, lognu, logalpha, logbeta = tf.split(outputs, len(self._map), axis=-1)
         nu = tf.nn.softplus(lognu)
         alpha = tf.nn.softplus(logalpha) + 1
         beta = tf.nn.softplus(logbeta)
         return tf.concat([gamma, nu, alpha, beta], axis=-1)
 
 
+    # Output: Shape(batch_size, n_recast_outputs)
+    @tf.function
+    def recast_to_prediction_epistemic_aleatoric(self, outputs):
+        gamma_indices = [ii for ii in range(self._map['gamma'] * self.units, self._map['gamma'] * self.units + self.units + 1)]
+        nu_indices = [ii for ii in range(self._map['nu'] * self.units, self._map['nu'] * self.units + self.units + 1)]
+        alpha_indices = [ii for ii in range(self._map['alpha'] * self.units, self._map['alpha'] * self.units + self.units + 1)]
+        beta_indices = [ii for ii in range(self._map['beta'] * self.units, self._map['beta'] * self.units + self.units + 1)]
+        prediction = tf.gather(output, indices=gamma_indices, axis=-1)
+        ones = tf.ones(tf.shape(prediction), dtype=output.dtype)
+        aleatoric = tf.math.divide(tf.gather(output, indices=beta_indices, axis=-1), tf.math.subtract(tf.gather(output, indices=alpha_indices, axis=-1), ones))
+        epistemic = tf.math.divide(aleatoric, tf.gather(output, indices=nu_indices, axis=-1))
+        return tf.concat([prediction, epistemic, aleatoric], axis=-1)
+
+
+    # Output: Shape(batch_size, n_recast_outputs)
+    @tf.function
+    def _recast(self, outputs):
+        return self.recast_to_prediction_epistemic_aleatoric(outputs)
+
+
     def compute_output_shape(self, input_shape):
-        return tf.Shape([input_shape[0], 4 * self.units])
+        return tf.Shape([input_shape[0], self._n_outputs])
 
 
     def get_config(self):
@@ -40,60 +75,24 @@ class DenseReparameterizationNormalInverseGamma(tf.keras.layers.Layer):
         return {**base_config, **config}
 
 
-class EvidentialBNN(tf.keras.models.Model):
 
-    def __init__(self, n_output, n_hidden, n_special):
-        super(EvidentialBNN, self).__init__()
-        self.n_outputs = n_output
-        self.n_hiddens = list(n_hidden) if isinstance(n_hidden, (list, tuple)) else [20]
-        self.n_specials = list(n_special) if isinstance(n_special, (list, tuple)) else [5]
-        while len(self.n_specials) < self.n_outputs:
-            self.n_specials.append(self.n_specials[-1])
-        if len(self.n_specials) > self.n_outputs:
-            self.n_specials = self.n_specials[:self.n_outputs]
-
-        self.base_activation = LeakyReLU(alpha=0.2)
-
-        self.common_layers = tf.keras.Sequential()
-        for ii in range(len(self.n_hidden)):
-            self.common_layers.add(Dense(self.n_hidden[ii], activation=self.base_activation, name=f'common{ii}')
-
-        self.output_channels = [None] * self.n_outputs
-        for jj in range(self.n_outputs):
-            channel = tf.keras.Sequential()
-            channel.add(Dense(self.n_specials[jj], activation=self.base_activation, name=f'specialized{jj}')
-            channel.add(DenseReparameterizationNormalInverseGamma(1, name=f'output{jj}')
-            self.output_channels[jj] = channel
-
-
-    def call(self, inputs):
-        commons = self.common_layers(inputs)
-        specials = []
-        for jj in range(len(self.output_channels)):
-            specials.append(self.output_channels[jj](commons))
-        outputs = Concatenate(specials)
-        return outputs
-
-
-    def get_config(self):
-        base_config = super(EvidentialBNN, self).get_config()
-        config = {
-            'n_output': self.n_outputs,
-            'n_hidden': self.n_hiddens,
-            'n_special': self.n_specials
-        }
-        return {**base_config, **config}
+# ------ LOSSES ------
 
 
 class NLLNormalInverseGammaLoss(tf.keras.losses.Loss):
 
+
     def __init__(self, weight=1.0, **kwargs):
+
         super(NLLNormalInverseGammaLoss, self).__init__(**kwargs)
+
         self.weight = weight if isinstance(weight, float) else 1.0
 
 
-    def call(self, targets, gammas, nus, alphas, betas):
+    @tf.function
+    def call(self, target_values, distribution_moments):
         weight = tf.constant(self.weight, dtype=self.dtype)
+        gammas, nus, alphas, betas = tf.unstack(distribution_moments, axis=-1)
         omegas = 2.0 * betas * (1.0 + nus)
         loss = (
             0.5 * tf.math.log(np.pi / nus) -
@@ -117,7 +116,51 @@ class NLLNormalInverseGammaLoss(tf.keras.losses.Loss):
 
 
 
-def create_model(n_input, n_output, n_hidden, n_special):
-    model = EvidentialBNN(n_output, n_hidden, n_special)
-    model.build((None, n_input))
-    return model
+class EvidenceRegularizationLoss(tf.keras.losses.Loss):
+
+
+    def __init__(self, weight=1.0, **kwargs):
+
+        super(EvidenceRegularizationLoss, self).__init__(**kwargs)
+
+        self.weight = weight if isinstance(weight, float) else 1.0
+
+
+    @tf.function
+    def call(self, target_values, distribution_moments):
+        weight = tf.constant(self.weight, dtype=self.dtype)
+        gammas, nus, alphas, betas = tf.unstack(distribution_moments, axis=-1)
+        loss = tf.math.abs(target_values - gammas) * (2.0 * nus + alphas)
+        if self.reduction == 'mean':
+            loss = tf.reduce_mean(loss, axis=0)
+        elif self.reduction == 'sum':
+            loss = tf.reduce_sum(loss, axis=0)
+        return loss
+
+
+    def get_config(self):
+        base_config = super(EvidenceRegularizationLoss, self).get_config()
+        config = {
+            'weight': self.weight
+        }
+        return {**base_config, **config}
+
+
+
+class EvidentialLoss(tf.keras.losses.Loss):
+
+
+    def __init__(self, **kwargs):
+
+        super(EvidentialLoss, self).__init__(**kwargs)
+
+
+
+class MultiOutputEvidentialLoss(tf.keras.losses.Loss):
+
+
+    def __init__(self, **kwargs):
+
+        super(EvidentialLoss, self).__init__(**kwargs)
+
+
