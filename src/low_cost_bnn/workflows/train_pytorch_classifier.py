04e3bb8ec59bf4b41b116f6d67fdfb919f60c11d
Aaron Ho <aaronkho@mit.edu>
2024-06-28 16:30:29 -0400

    Added placeholders for PyTorch SNGP classifier implementation

---
+++ src/low_cost_bnn/workflows/train_pytorch_classifier.py
@@ -0,0 +1,19 @@
+import argparse
+import time
+import logging
+import json
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import torch
+from ..utils.pipeline_tools import setup_logging, print_settings
+from ..utils.helpers_pytorch import default_dtype, save_model
+
+
+def main():
+    raise NotImplementedError('Workflow not implemented yet!')
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/low_cost_bnn/workflows/train_pytorch_sngp.py
@@ -0,0 +1,20 @@
+import argparse
+import time
+import copy
+import logging
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import torch
+import torch.distributions as tnd
+from ..utils.pipeline_tools import setup_logging, print_settings, preprocess_data
+from ..utils.helpers import mean_absolute_error, mean_squared_error
+
+
+def main():
+    raise NotImplementedError('Workflow not implemented yet!')
+
+
+if __name__ == "__main__":
+    main()
+
