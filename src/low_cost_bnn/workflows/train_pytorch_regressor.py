06bb2d078f43f456aa21250d897687d929580296
aaronkho <aaronkho@mit.edu>
2025-08-05 23:25:50 -0400

    Extended gradient clipping to PyTorch NCP workflow

--- src/low_cost_bnn/workflows/train_pytorch_regressor.py
+++ src/low_cost_bnn/workflows/train_pytorch_regressor.py
@@ -109,6 +109,7 @@ def launch_pytorch_regressor_pipeline(
             batch_size=specs.get('batch_size', None),
             early_stopping=specs.get('early_stopping', None),
             minimum_performance=specs.get('minimum_performance', None),
+            maximum_gradient=specs.get('maximum_gradient', None),
             shuffle_seed=specs.get('shuffle_seed', None),
             sample_seed=specs.get('sample_seed', None),
             generalized_widths=specs.get('generalized_node', None),
