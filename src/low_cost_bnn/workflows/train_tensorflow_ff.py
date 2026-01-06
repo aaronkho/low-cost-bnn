570e81b193a63e91e135bc0a52436c702551f081
aaronkho <aaronkho@mit.edu>
2026-01-06 18:04:28 -0500

    Normalized feedforward display metrics for easier comparison

--- src/low_cost_bnn/workflows/train_tensorflow_ff.py
+++ src/low_cost_bnn/workflows/train_tensorflow_ff.py
@@ -525,8 +525,8 @@ def train_tensorflow_feedforward(
 
         train_total = train_loss_trackers['total'].result().numpy()
         train_reg = train_loss_trackers['reg'].result().numpy()
-        train_se = np.array([tracker.result().numpy() for tracker in train_loss_trackers['se']])
-        train_rse = np.array([tracker.result().numpy() for tracker in train_loss_trackers['rse']])
+        train_se = np.array([tracker.result().numpy() for tracker in train_loss_trackers['se']]) / float(train_length)
+        train_rse = np.array([tracker.result().numpy() for tracker in train_loss_trackers['rse']]) / float(train_length)
         train_sae = np.array([tracker.result().numpy() for tracker in train_performance_trackers['sae']])
         train_sse = np.array([tracker.result().numpy() for tracker in train_performance_trackers['sse']])
         train_sst = np.array([tracker.result().numpy() for tracker in train_performance_trackers['sst']])
@@ -571,8 +571,8 @@ def train_tensorflow_feedforward(
 
         valid_total = valid_loss_trackers['total'].result().numpy()
         valid_reg = valid_loss_trackers['reg'].result().numpy() * float(valid_length) / float(train_length) # Invariant to batch size, needed for comparison
-        valid_se = np.array([tracker.result().numpy() for tracker in valid_loss_trackers['se']])
-        valid_rse = np.array([tracker.result().numpy() for tracker in valid_loss_trackers['rse']])
+        valid_se = np.array([tracker.result().numpy() for tracker in valid_loss_trackers['se']]) / float(valid_length)
+        valid_rse = np.array([tracker.result().numpy() for tracker in valid_loss_trackers['rse']]) / float(valid_length)
         valid_sae = np.array([tracker.result().numpy() for tracker in valid_performance_trackers['sae']])
         valid_sse = np.array([tracker.result().numpy() for tracker in valid_performance_trackers['sse']])
         valid_sst = np.array([tracker.result().numpy() for tracker in valid_performance_trackers['sst']])
